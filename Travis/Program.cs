63176fe0446fa97a0f3582519af2a011d45d6062
Michael E Miles <michael@miles-me.net>
2017-05-09 10:59:22 -0500

    This program is much smaller in Python. I think the fact that is it not strongly typed makes a difference plus there some better string methods.

--- Travis/Program.cs
+++ Travis/Program.cs
@@ -1,15 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Travis
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            var knownUsers = new List<string>() {"Alice", "Bob", "Claire", "Dan", "Emma", "Fred", "Georgie", "Harry"};
+            var travisContinue = true;
+
+            while (travisContinue)
+            {
+                Console.WriteLine("Hi my name is Travis");
+                Console.Write("What is your name?: ");
+
+                var answer = Console.ReadLine();
+                if (!string.IsNullOrEmpty(answer)) 
+                {
+                    answer = answer.ToLower();
+                    var name = FirstToUpper(answer);
+
+                    if (knownUsers.Contains(name))
+                    {
+                        Console.WriteLine($"\nHello {name}!");
+                        Console.Write("Would you like to be removed from the system <y/n>?: ");
+                        var remove = Console.ReadLine();
+
+                        if (!string.IsNullOrEmpty(remove))
+                        {
+                            if (remove == "y")
+                            {
+                                knownUsers.Remove(name);
+                                Console.WriteLine($"\nYou have been removed {name}\n");
+                            }
+                            else if (remove == "n")
+                                Console.WriteLine("\nNo problem, I didn't want you to leave anyway!\n");
+                        }
+                        else
+                            travisContinue = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nHmmm I don't think I know you yet {name}.");
+                        Console.Write("Would you like to be added to the system <y/n>?: ");
+                        var addName = Console.ReadLine();
+
+                        if (!string.IsNullOrEmpty(addName))
+                        {
+                            if (addName == "y")
+                            {
+                                knownUsers.Add(name);
+                                Console.WriteLine($"\n{name}, you have been added to the system.\n");
+                                foreach (var temp in knownUsers)
+                                {
+                                    Console.WriteLine(temp);
+                                }
+                                Console.WriteLine("\n");
+                            }
+                            else if (addName == "n")
+                            {
+                                Console.WriteLine("\nNo Worries, see you around\n");
+                            }
+                        }
+                        else
+                        {
+                            travisContinue = false;
+                        }
+                    }
+                }
+                else
+                    travisContinue = false;
+            }
+        }
+
+        private static string FirstToUpper(string text)
+        {
+            var firstLetter = text[0].ToString().ToUpper();
+            return firstLetter + text.Substring(1);
         }
     }
 }
